a50f15b1709ef1149f27afa0a3351e2c2750458b
dspray95 <dspray95@gmail.com>
2026-02-21 21:29:06 +0000

    prod guard

--- src/sections/RepaymentResultsSplashSection/hooks/useSubmitResults.ts
+++ src/sections/RepaymentResultsSplashSection/hooks/useSubmitResults.ts
@@ -65,7 +65,7 @@ export const useSubmitResults = () => {
     futureIncomeMode,
   } = useLoanCalculatorStore();
 
-  const isProduction = true;
+  const isProduction = import.meta.env.PROD;
   const hasResults = !!undergraduateRepaymentPlan && !!loanFormValues;
 
   useEffect(() => {
